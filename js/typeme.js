9e6f5b5bdce059218221edaeb817127291a8d63e
Tai Doan <tdoan1@spu.edu>
2019-11-28 04:12:05 -0800

    Changes

--- js/typeme.js
+++ js/typeme.js
@@ -2,7 +2,7 @@ var typed = $(".typed");
 
 $(() => {
 	typed.typed({
-	  strings: ["programming.", "designing.",  "singing.", "playing guitar.", "waiting for a bus."],
+	  strings: ["programming.", "designing.",  "playing soccer.", "cooking.", "being around with fun people."],
 	  typeSpeed: 140,
 	  loop: true,
 	});
